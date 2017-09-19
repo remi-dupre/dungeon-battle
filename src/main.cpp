a48f2b5037fbf7af8d141a5a2095fa71c9633829
Esum <benjamin.graillot@hotmail.com>
2017-09-19 17:12:08 +0200

    Added documentation for function main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <SFML/Graphics.hpp>
 
-int main()
+/**
+ * \brief Main function
+ */
+int main(int argc, char *argv[])
 {
     sf::RenderWindow window({800, 600}, "Dungeon Battle", sf::Style::Close);
     window.setVerticalSyncEnabled(true);
