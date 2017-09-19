52ca1ca368adf95ab24b3afbead37a8de747e4c7
Jean-Baptiste Daval <krokmou@crans.org>
2017-09-19 20:21:50 +0200

    Update config.cpp

--- src/config.cpp
+++ src/config.cpp
@@ -4,7 +4,7 @@
 
 #include "config.hpp"
 
-
+// Default configuration
 Configuration::Configuration() :
     width(800),
     height(600),
@@ -17,26 +17,33 @@ void Configuration::parse(const std::string& filename)
     std::ifstream config_file(filename);
     std::string line;
 
+    if (!config_file.is_open())
+	return;
+
     while (std::getline(config_file, line))
     {
-        if (line[0] == '#' || line[0] == '[')
+        if (line[0] == '[')
             continue;
+	line = line.substr(0, line.find('#')); // Remove commentary at the end of line
 
         std::string::size_type eq_pos = line.find('=');
 
         if (eq_pos == std::string::npos)
             continue;
+	if (eq_pos + 1 >= line.size())
+	    continue;
 
         std::string option_name = line.substr(0, eq_pos);
+	std::string value = line.substr(eq_pos+1, line.size());
 
-        if(option_name == "height")
-            height = std::stoi(line.substr(eq_pos+1, line.size()));
+        if (option_name == "height")
+            height = std::stoi(value);
         else if (option_name == "width")
-            width = std::stoi(line.substr(eq_pos+1, line.size()));
+            width = std::stoi(value);
         else if (option_name ==  "fullscreen")
-            fullscreen = std::stoi(line.substr(eq_pos+1, line.size()));
+            fullscreen = std::stoi(value);
         else if (option_name ==  "vsync")
-            vsync = std::stoi(line.substr(eq_pos+1, line.size()));
+            vsync = std::stoi(value);
     }
 
     return;
