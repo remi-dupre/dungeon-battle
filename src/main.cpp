afdb300c32e5992bdeedf506ce4e418b34a6bfb4
Esum <benjamin.graillot@hotmail.com>
2017-09-20 08:33:30 +0200

    Fixed style

--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,7 @@ int main(int argc, char *argv[])
     while (window.isOpen())
     {
         sf::Event event;
+        
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
