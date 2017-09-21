db819eb44b2874e947ff1fa25ea276575d70eb30
Esum <benjamin.graillot@hotmail.com>
2017-09-21 16:09:27 +0200

    Downgrade to SFML 2.1

--- install_sfml.sh
+++
@@ -1,5 +0,0 @@
-wget --quiet https://www.sfml-dev.org/files/SFML-2.4.2-linux-gcc-64-bit.tar.gz
-tar -xf SFML-2.4.2-linux-gcc-64-bit.tar.gz
-mv SFML-2.4.2 SFML
-cp SFML/lib/*.so.2.4 .
-cp SFML/lib/*.so.2.4 tests
