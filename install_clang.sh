2e8f61bc440dedbcbcf7642ce6133e930ac536b6
Esum <benjamin.graillot@hotmail.com>
2017-09-21 14:01:32 +0200

    Download clang quietly

--- install_clang.sh
+++ install_clang.sh
@@ -1,3 +1,3 @@
-wget http://releases.llvm.org/5.0.0/clang+llvm-5.0.0-linux-x86_64-ubuntu14.04.tar.xz > /dev/null
+wget --quiet http://releases.llvm.org/5.0.0/clang+llvm-5.0.0-linux-x86_64-ubuntu14.04.tar.xz
 tar -xf clang+llvm-5.0.0-linux-x86_64-ubuntu14.04.tar.xz
 mv clang+llvm-5.0.0-linux-x86_64-ubuntu14.04 clang
