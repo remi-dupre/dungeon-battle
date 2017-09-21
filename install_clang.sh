65a0203066f8d4c91ef4a4861baadbae3678b070
Esum <benjamin.graillot@hotmail.com>
2017-09-21 14:50:01 +0200

    Changed SFML installation method

--- install_clang.sh
+++ install_clang.sh
@@ -1,4 +1,4 @@
 wget --quiet http://releases.llvm.org/5.0.0/clang+llvm-5.0.0-linux-x86_64-ubuntu14.04.tar.xz
 tar -xf clang+llvm-5.0.0-linux-x86_64-ubuntu14.04.tar.xz
 mv clang+llvm-5.0.0-linux-x86_64-ubuntu14.04 clang
-PATH=${PATH}:${PWD}/clang/bin
+PATH=${PWD}/clang/bin:${PATH}:${PWD}/clang/bin
