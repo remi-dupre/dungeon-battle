865071961de677dfc79d1ddd5571fb266a36361e
Esum <benjamin.graillot@hotmail.com>
2017-09-21 14:35:18 +0200

    Compile with SFML

--- install_sfml.sh
+++ install_sfml.sh
@@ -1,4 +1,5 @@
 wget --quiet https://www.sfml-dev.org/files/SFML-2.4.2-linux-gcc-64-bit.tar.gz
 tar -xf SFML-2.4.2-linux-gcc-64-bit.tar.gz
-rsync -avq SFML-2.4.2/ clang/
-rmdir SFML-2.4.2
+mv SFML-2.4.2 SFML
+mv SFML/include/SFML clang/include/c++/v1
+mv SFML/lib/* clang/lib
