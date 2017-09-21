5e11824bc86cc6499e82ac77879e3c9f88d453ad
Esum <benjamin.graillot@hotmail.com>
2017-09-21 15:40:42 +0200

    Fixed compilation

--- install_sfml.sh
+++ install_sfml.sh
@@ -1,5 +1,5 @@
 wget --quiet https://www.sfml-dev.org/files/SFML-2.4.2-linux-gcc-64-bit.tar.gz
 tar -xf SFML-2.4.2-linux-gcc-64-bit.tar.gz
 mv SFML-2.4.2 SFML
-mv SFML/include/SFML clang/include/c++/v1
-mv SFML/lib/* clang/lib
+cp SFML/lib/*.so.2.4 .
+cp SFML/lib/*.so.2.4 tests
