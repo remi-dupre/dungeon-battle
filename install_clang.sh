993eaaa400891fc67f80d7f0778e25c66469afea
Esum <benjamin.graillot@hotmail.com>
2017-09-21 18:40:59 +0200

    Removed install_clang.sh

--- install_clang.sh
+++
@@ -1,3 +0,0 @@
-wget --quiet http://releases.llvm.org/5.0.0/clang+llvm-5.0.0-linux-x86_64-ubuntu14.04.tar.xz
-tar -xf clang+llvm-5.0.0-linux-x86_64-ubuntu14.04.tar.xz
-mv clang+llvm-5.0.0-linux-x86_64-ubuntu14.04 clang
