582c1b7f39d880df89df048ec35a8061f426f4f1
Jean-Baptiste Daval <krokmou@crans.org>
2017-09-29 19:18:13 +0200

    Fixed style

--- src/entity.cpp
+++ src/entity.cpp
@@ -1,13 +1,11 @@
 #include "entity.hpp"
 
-Entity::Entity(sf::Vector2i _position,
-               Direction _orientation,
-               EntityType _type) :
+
+Entity::Entity(sf::Vector2i _position, Direction _orientation, EntityType _type) :
     position(_position),
     orientation(_orientation),
     type(_type)
 {}
-
 
 sf::Vector2i Entity::getPosition() const
 {
