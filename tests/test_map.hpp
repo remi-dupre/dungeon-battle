5dcd01cc041112a0a54914e6a3f3e4726d58ac19
Rémi Dupré <remim.dupre@gmail.com>
2017-10-20 15:21:26 +0200

    Splitted test files, `make lint`, simplified `make test`

--- tests/test_map.hpp
+++ tests/test_map.hpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <random>
 
+
 #include "../src/generation/pattern.hpp"
 #include "../src/generation/pattern.cpp"
 #include "../src/generation/room.hpp"
@@ -15,10 +16,6 @@
 #include "../src/generation/space.hpp"
 #include "../src/generation/space.cpp"
 
-#include "../src/config.hpp"
-#include "../src/config.cpp"
-#include "../src/args.hpp"
-#include "../src/args.cpp"
 #include "../src/map.hpp"
 #include "../src/map.cpp"
 #include "../src/entity.hpp"
@@ -26,76 +23,6 @@
 #include "../src/rand.hpp"
 #include "../src/utility.hpp"
 #include "../src/utility.cpp"
-
-
-#pragma GCC diagnostic ignored "-Wpragmas"
-#pragma GCC diagnostic ignored "-Wwrite-strings"
-#pragma GCC diagnostic ignored "-Wwritable-strings"
-
-
-class ConfigTester : public CxxTest::TestSuite
-{
-public:
-    void testFile1()
-    {
-        Configuration config;
-        config.read("tests/config1.ini");
-
-        TS_ASSERT_EQUALS(config.width, 800);
-        TS_ASSERT_EQUALS(config.height, 600);
-        TS_ASSERT_EQUALS(config.scalefactor, 1);
-        TS_ASSERT_EQUALS(config.fullscreen, false);
-        TS_ASSERT_EQUALS(config.vsync, true);
-        TS_ASSERT_EQUALS(config.maxfps, 60);
-    }
-
-    void testFile2()
-    {
-        Configuration config;
-        config.read("tests/config2.ini");
-
-        TS_ASSERT_EQUALS(config.width, 685);
-        TS_ASSERT_EQUALS(config.height, 534);
-        TS_ASSERT_EQUALS(config.scalefactor, 2);
-        TS_ASSERT_EQUALS(config.fullscreen, true);
-        TS_ASSERT_EQUALS(config.vsync, true);
-        TS_ASSERT_EQUALS(config.maxfps, 120);
-    }
-};
-
-
-class ParserTester : public CxxTest::TestSuite
-{
-public:
-    void testArgs1()
-    {
-        std::map<Option, std::string> options;
-
-        char *argv[] =
-        {
-            "",
-            "--config",
-            "config3.ini"
-        };
-
-        TS_ASSERT_EQUALS(parse_arguments(options, array_length(argv), argv), 0);
-        TS_ASSERT_EQUALS(options[Option::Config], "config3.ini");
-    }
-
-    void testArgs2()
-    {
-        std::map<Option, std::string> options;
-
-        char *argv[] =
-        {
-            "",
-            "--config=config2.ini"
-        };
-
-        TS_ASSERT_EQUALS(parse_arguments(options, array_length(argv), argv), 0);
-        TS_ASSERT_EQUALS(options[Option::Config], "config2.ini");
-    }
-};
 
 
 // Number of maps to generate for a test
