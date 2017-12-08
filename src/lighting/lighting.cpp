08a09e3166594ddbe525e2c040cfa73b8712c9e4
fnareoh <garance.gourdel@laposte.net>
2017-12-08 15:33:41 +0100

    lighting in progress

--- src/lighting/lighting.cpp
+++ src/lighting/lighting.cpp
@@ -12,7 +12,7 @@ double from_y_to_x(double a,double b,double c,double d,double y){
     return (y-d)*(a-c)/(b-d) + c;
 }
 
-vector<Vector2i> cell_on_the_way(Vector2i pos1, Vector2i pos2){
+vector<Vector2i> cell_on_the_way(Vector2i pos1, Vector2i pos2, vector<vector<int>> map){
     Vector2<double> cent1,cent2;
     cent1.x = (double) pos1.x + 0.5; cent1.y = (double) pos1.y + 0.5;
     cent2.x = (double) pos2.x + 0.5; cent2.y = (double) pos2.y + 0.5;
@@ -26,9 +26,19 @@ vector<Vector2i> cell_on_the_way(Vector2i pos1, Vector2i pos2){
     vector<Vector2i> list_cell; //size à definir ou faire des push back...
     for(int i = pos1.x; i <= pos2.x; i++){
         int j = floor(f_x(x));
-
+        if (map[i][j] == 0) break;
+        Vector2i cell_ij;
+        cell_ij.x = i; cell_ij.y = j;
+        list_cell.push_back(cell_ij);
     }
-
+    for(int j = pos1.y; j <= pos2.y; j++){
+        int i = floor(f_y(y));
+        if (map[i][j] == 0) break;
+        Vector2i cell_ij;
+        cell_ij.x = i; cell_ij.y = j;
+        list_cell.push_back(cell_ij);
+    }
+    return list_cell;
 }
 
 int main(){
